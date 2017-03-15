aa03e4b4aec3881ca3794f5d5e4f1bd750324baf
mukolamarkanuch <mukolamarkanuch@gmail.com>
2017-03-15 16:23:48 +0200

    backstage

---
+++ config/Migrations/20170315142200_AddTimestampsToMarks.php
@@ -0,0 +1,26 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddTimestampsToMarks extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('marks');
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->update();
+    }
+}
